89b53fdb359bf0a8cea2d2d79f34805cbaf7a5b8
Bartosz Rabij <bartosz.rabij@onet.pl>
2024-02-05 20:25:39 +0100

    Report page data bindings

---
+++ BusTransport.Mob/ViewModels/ReportProblemViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BusTransport.Mob.TempModels;
+using System.Collections.ObjectModel;
+
+namespace BusTransport.Mob.ViewModels
+{
+    public class ReportProblemViewModel
+    {
+        public ObservableCollection<EventActionCategory> EventActionCategories { get; }
+        public ReportProblemViewModel()
+        {
+            EventActionCategories = new ObservableCollection<EventActionCategory>
+            {
+                new EventActionCategory { Name = "Category 1" },
+                new EventActionCategory { Name = "Category 2" },
+            };
+        }
+    }
+}
