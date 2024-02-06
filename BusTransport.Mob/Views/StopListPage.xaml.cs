b2d1823daee7de221d888b600d5674d6db941e72
Bartosz Rabij <bartosz.rabij@onet.pl>
2024-02-06 21:31:31 +0100

    Added BusStop ScrollView to StopListPage

--- BusTransport.Mob/Views/StopListPage.xaml.cs
+++ BusTransport.Mob/Views/StopListPage.xaml.cs
@@ -1,9 +1,15 @@
+using BusTransport.Mob.ViewModels;
+
 namespace BusTransport.Mob.Views;
 
 public partial class StopListPage : ContentPage
 {
-	public StopListPage()
+    private BusStopViewModel _viewModel;
+    public StopListPage()
 	{
 		InitializeComponent();
-	}
+        _viewModel = new BusStopViewModel();
+        this.BindingContext = _viewModel;
+    }
+
 }
