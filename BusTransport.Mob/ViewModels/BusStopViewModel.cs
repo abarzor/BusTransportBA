df6d6954088c9be0078ee212b8a1d206a86e6387
Bartosz Rabij <bartosz.rabij@onet.pl>
2024-02-06 21:18:48 +0100

    Added BusStopViewModel

---
+++ BusTransport.Mob/ViewModels/BusStopViewModel.cs
@@ -0,0 +1,19 @@
+﻿using BusTransport.Model.Models;
+using System.Collections.ObjectModel;
+
+namespace BusTransport.Mob.ViewModels
+{
+    public class BusStopViewModel
+    {
+        public ObservableCollection<BusStop> BusStops { get; set; }
+        public BusStopViewModel()
+        {
+            BusStops = new ObservableCollection<BusStop>
+            {
+                new BusStop { StopName = "Stop 1", Arrival = new TimeOnly(8, 0), Departure = new TimeOnly(8, 5) },
+                new BusStop { StopName = "Stop 2", Arrival = new TimeOnly(8, 15), Departure = new TimeOnly(8, 20) },
+                new BusStop { StopName = "Stop 3", Arrival = new TimeOnly(8, 30), Departure = new TimeOnly(8, 35) },
+            };
+        }
+    }
+}
--- BusTransport.Model/Models/BusStop.cs
+++ BusTransport.Model/Models/BusStop.cs
@@ -6,6 +6,8 @@ namespace BusTransport.Model.Models
     {
         public int BusStop_Id { get; set; }
         public string StopName { get; set; }
+        public TimeOnly Arrival { get; set; }
+        public TimeOnly Departure { get; set; }
         public double BusStopLocationX { get; set; }
         public double BusStopLocationY { get; set; }
     }
